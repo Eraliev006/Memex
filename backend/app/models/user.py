8655ab55ae27aff4dfbdb332f0030acc46988742
Eraliev006 <eraliev.adilet006@gmail.com>
2026-05-23 11:36:20 +0200

    feat: add user models

---
+++ backend/app/models/user.py
@@ -0,0 +1,17 @@
+import uuid
+
+from datetime import datetime
+
+from sqlalchemy import UUID, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+from backend.app.models.base import Base
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
+    name: Mapped[str] = mapped_column(nullable=False)
+    email: Mapped[str] = mapped_column(unique=True, index=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(nullable=False)
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now(),nullable=False)
