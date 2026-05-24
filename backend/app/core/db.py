52bb66f146fe10d6a116686186eac3cfe5e0475b
Eraliev006 <eraliev.adilet006@gmail.com>
2026-05-24 19:22:09 +0200

    refactor: refactored import

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.asyncio import create_async_engine
 
-from backend.app.core import settings
+from app.core import settings
 
 engine = create_async_engine(str(settings.SQLALCHEMY_DATABASE_URI))
