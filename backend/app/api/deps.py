4857b86a5baa735da17db3a43978862f06551af0
Eraliev006 <eraliev.adilet006@gmail.com>
2026-05-28 01:08:41 +0600

    feat: add auth routes

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -1,8 +1,11 @@
 
+from fastapi import Depends
 from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncSession
 
 from app.core import engine
-from typing import AsyncGenerator
+from typing import Annotated, AsyncGenerator
+
+from app.services.auth import AuthService
 
 async_session_maker = async_sessionmaker(
     bind=engine,
@@ -12,3 +15,10 @@ async_session_maker = async_sessionmaker(
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         yield session
+        
+SessionDep = Annotated[AsyncSession, Depends(get_db)]
+
+# AUTH SERVICE DI
+async def get_auth_service(db: SessionDep) -> AuthService:
+    return AuthService(db)
+AuthServiceDep = Annotated[AuthService, Depends(get_auth_service)]
