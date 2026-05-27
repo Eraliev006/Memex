5a4cd4efb20fd4c8f467b24adad082daa443517c
Eraliev006 <eraliev.adilet006@gmail.com>
2026-05-28 00:51:02 +0600

    feat: add schemas for reset password

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -2,5 +2,14 @@
 
 from .auth_schemas import RegisterRequest, LoginRequest, TokenResponse
 from .user import UserCreate, UserResponse
+from .reset_password import ResetPasswordRequest, ResetPasswordResponse
 
-__all__ = ['RegisterRequest', 'LoginRequest', 'TokenResponse', 'UserCreate', 'UserResponse']
+__all__ = [
+    'RegisterRequest',
+    'LoginRequest',
+    'TokenResponse',
+    'UserCreate',
+    'UserResponse',
+    'ResetPasswordRequest',
+    'ResetPasswordResponse',
+    ]
