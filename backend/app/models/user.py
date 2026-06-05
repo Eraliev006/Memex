65be2619e4c8382e972323ed968af4235475ee7e
Eraliev006 <eraliev.adilet006@gmail.com>
2026-06-05 15:09:21 +0600

    feat: add realtion to document

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,11 +1,15 @@
+from typing import TYPE_CHECKING
 import uuid
 
 from datetime import datetime
 
 from sqlalchemy import UUID, func
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.models.base import Base
+
+if TYPE_CHECKING:
+    from .document import Document
 
 class User(Base):
     __tablename__ = "users"
@@ -15,3 +19,5 @@ class User(Base):
     email: Mapped[str] = mapped_column(unique=True, index=True, nullable=False)
     hashed_password: Mapped[str] = mapped_column(nullable=False)
     created_at: Mapped[datetime] = mapped_column(server_default=func.now(),nullable=False)
+    
+    documents: Mapped['Document'] = relationship(back_populates='user')
