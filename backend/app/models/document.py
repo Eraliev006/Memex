91a967c53d38cafa2991244382af81f4d0dbb538
Eraliev006 <eraliev.adilet006@gmail.com>
2026-06-05 15:09:01 +0600

    feat: add document model

---
+++ backend/app/models/document.py
@@ -0,0 +1,34 @@
+
+
+from datetime import datetime
+import enum
+from typing import TYPE_CHECKING
+import uuid
+
+from sqlalchemy import UUID, Enum, ForeignKey, func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+if TYPE_CHECKING:
+    from .user import User
+
+
+class Document:
+    __tablename__ = 'documents'
+    
+    class Statuses(enum.Enum):
+        ready = 'ready'
+        processing = 'processing'
+        pending = 'pending'
+        failed = 'failed'
+    
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, index=True, default=(uuid.uuid4))
+    title: Mapped[str] = mapped_column(nullable=False)
+    user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+    original_filename: Mapped[str] = mapped_column(nullable=False)
+    storage_path: Mapped[str] = mapped_column(nullable=False)
+    content: Mapped[str | None]
+    status: Mapped[Statuses] = mapped_column(Enum)
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now(),nullable=False)
+    
+    
+    user: Mapped['User'] = relationship(back_populates='documents')
