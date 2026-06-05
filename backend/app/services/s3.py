b7604f30be69abe8c95e3af9f5b19eddf90e50c8
Eraliev006 <eraliev.adilet006@gmail.com>
2026-06-06 03:40:07 +0600

    feat: add s3 client service

--- backend/app/services/s3.py
+++ backend/app/services/s3.py
@@ -8,7 +8,16 @@ class S3Storage:
         self.session = get_session()
         self.bucket_name = settings.S3_BUCKET_NAME
         
-    async def upload_documents(self, file: UploadFile, object_key: str) -> str:
+    def _client_params(self):
+        return {
+            "service_name": "s3",
+            "endpoint_url": settings.S3_ENDPOINT_URL,
+            "aws_access_key_id": settings.S3_ACCESS_KEY,
+            "aws_secret_access_key": settings.S3_SECRET_KEY,
+            "region_name": "us-east-1"
+        }
+        
+    async def upload_documents(self, file_bytes: bytes, object_key: str, content_type: str = "application/pdf") -> str:
         """Upload document in S3 buckets
 
         Args:
@@ -19,20 +28,26 @@ class S3Storage:
             str: return upload path
         """
         
-        async with self.session.create_client(
-            's3',
-            endpoint_url = settings.S3_ENDPOINT_URL,
-            aws_access_key_id = settings.S3_ACCESS_KEY,
-            aws_secret_access_key = settings.S3_SECRET_KEY,
-            region_name="us-east-1"
-        ) as client:
-            file_content = await file.read()
-            
+        async with self.session.create_client(**self._client_params()) as client:
             await client.put_object(
                 Bucket=self.bucket_name,
                 Key=object_key,
-                Body=file_content,
-                ContentType=file.content_type
+                Body=file_bytes,
+                ContentType=content_type
             ) # type: ignore
             
             return object_key
+    
+    async def download_documents(self, storage_path: str):
+        """Download documents from S3 storage by storage path
+
+        Args:
+            storage_path (str): document's path in storage
+        """
+        async with self.session.create_client(**self._client_params()) as client:
+            response = await client.get_object(
+                Bucket=self.bucket_name,
+                Key=storage_path
+            ) # type: ignore
+            
+            return await response['Body'].read() # type: ignore
