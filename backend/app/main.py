e44783261f06afd088816e69b1759ef6913abd8e
Eraliev006 <eraliev.adilet006@gmail.com>
2026-06-08 12:32:12 +0600

    feat: add lifespan for ensuring that collection exists

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,23 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from app.core import settings
 from app.api.main import api_router
+from app.services.qdrant import QdrantService
 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    qdrant = QdrantService()
+    await qdrant.ensure_collection()
+    yield
+    
+    
 app = FastAPI(
     title=settings.PROJECT_NAME,
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    lifespan=lifespan
 )
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
