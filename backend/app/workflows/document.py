2eb47a41479f9c5335d7f6715601cdea11fb8d5d
Eraliev006 <eraliev.adilet006@gmail.com>
2026-06-08 12:31:57 +0600

    feat: inegrate chunking embeding and saving to qdrant into workflows

--- backend/app/workflows/document.py
+++ backend/app/workflows/document.py
@@ -1,15 +1,25 @@
 import uuid
 
+from qdrant_client.models import PointStruct
+
 from app.repositories.document import DocumentRepository
 from app.services.s3 import S3Storage
 from app.services.parser import LlamaParser
 from app.enums.document import DocumentStatuses
+from app.services import ChunkingService, EmbeddingService, QdrantService
+from app.api.deps import get_embeddings_providers
+from app.repositories.chunk import ChunkRepository
+
 
 class DocumentWorkflows:
     @staticmethod
     async def run(doc_id: uuid.UUID, async_session):
         repo = DocumentRepository(async_session)
         s3 = S3Storage()
+        embedder = EmbeddingService(provider=get_embeddings_providers())
+        chunking = ChunkingService()
+        qdrant = QdrantService()
+        chunk_repo = ChunkRepository(async_session)
         
         started = await repo.try_start_processing(doc_id)
         
@@ -29,6 +39,45 @@ class DocumentWorkflows:
                 file_bytes=file_bytes,
                 filename=doc.storage_path.split('/')[-1]
             )
+            
+            chunks = chunking.split(text if text else "")
+            if not chunks:
+                raise ValueError("Document has no content after parsing")
+            
+            embeddings: list[list[float]] = await embedder.create_embeddings(chunks)
+            
+            points = []
+            chunk_dicts = [] 
+            
+            for i, (chunk, embedding) in enumerate(zip(chunks, embeddings)):
+                point_id = uuid.uuid4()
+
+                point = PointStruct(
+                    id=str(point_id),
+                    vector=embedding,
+                    payload={
+                        "document_id": str(doc.id),
+                        "user_id": str(doc.user_id),
+                        "chunk_index": i,
+                        "content": chunk,
+                        "document_title": doc.title
+                    }
+                )
+
+                chunk_dict = {
+                    "id": uuid.uuid4(),
+                    "document_id": doc.id,
+                    "user_id": doc.user_id,
+                    "content": chunk,
+                    "chunk_index": i,
+                    "qdrant_point_id": point_id
+                }
+                
+                chunk_dicts.append(chunk_dict)
+                points.append(point)
+                
+            await qdrant.upsert_points(points)
+            await chunk_repo.bulk_create(chunk_dicts)
             
             await repo.update_document(
                 id=doc_id,
